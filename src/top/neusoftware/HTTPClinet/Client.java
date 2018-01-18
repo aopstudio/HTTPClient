551dcd6df2b7a05a3c904c110b26328f045f737d
aopstudio <cenzhedong2@126.com>
2018-01-18 16:13:27 +0800

    奇怪的文件传输

--- src/top/neusoftware/HTTPClinet/Client.java
+++ src/top/neusoftware/HTTPClinet/Client.java
@@ -1,21 +1,28 @@
 package top.neusoftware.HTTPClinet;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
-	Socket s;
-	PrintStream writer;
-	InputStream in;
-	InputStreamReader reader;
+	private Socket s;
+	private PrintWriter writer;
+	private InputStream in;
+	private InputStreamReader reader;
+	String saveLocation="D:/FTCache";
 	
-	public void Client() throws UnknownHostException, IOException {
+	public Client() throws UnknownHostException, IOException {
 		s=new Socket("127.0.0.1",8888);
-		writer=new PrintStream(s.getOutputStream());
+		writer=new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
 		in=s.getInputStream();
 		reader=new InputStreamReader(in);
 	}
@@ -28,17 +35,45 @@ public class Client {
 		writer.flush();
 	}
 	
-	public void receiveResponse() throws IOException {
-		char ch;
+	public void receiveResponse(String fileName) throws IOException {
+		char ch='\0';
+		BufferedReader stringReader = new BufferedReader(new InputStreamReader(in));
 		StringBuilder res=new StringBuilder();
+		String statusCode;
 		String firstLineOfResponse;
-		String secondLineOfResponse;
-		String threeLineOfResponse;
-		String fourLineOfResponse;
-
 		while((ch=(char) reader.read())!='\n') {
 			res.append(ch);
 		}
-		res.append(ch);
+		res.append(ch);		
+		firstLineOfResponse=res.toString();
+		statusCode=firstLineOfResponse.split(" ")[1];//第一行格式是HTTP/1.1 200 OK或者HTTP/1.1 404 Not Found，
+		//statusCode就是中间那个数字
+		if(statusCode.equals("200")) {
+			while(true) {
+				if(ch=='\n'&(ch=(char)reader.read())=='\r')
+					break;//读到前一个\n后面紧接着\r，代表是空行，跳出循环去读后面的具体文件内容
+			}
+			byte[] b = new byte[1024];
+			OutputStream out = new FileOutputStream(saveLocation+"/"+fileName);
+			int len;//读后面的数据
+			while((len=in.read(b))!=-1)
+			{		    	   
+				out.write(b, 0, len);
+				out.flush();
+			}
+			in.close();
+			out.close();
+			
+		}
+		else if(statusCode.equals("404")) {
+			StringBuffer result = new StringBuffer();
+			String line;
+			while ((line = stringReader.readLine()) != null) {
+					result.append(line);
+			}
+			reader.close();
+			System.out.println(result);
+		}
+		
 	}
 }
--- src/top/neusoftware/HTTPClinet/ClientService.java
+++ src/top/neusoftware/HTTPClinet/ClientService.java
@@ -1,7 +1,12 @@
 package top.neusoftware.HTTPClinet;
 
+import java.io.IOException;
+
 public class ClientService {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		Client cli=new Client();
+		String fileName="cookie.txt";
+		cli.sendRequest(fileName);
+		cli.receiveResponse(fileName);
 	}
 }
